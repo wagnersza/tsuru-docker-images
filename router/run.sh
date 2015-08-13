1db1865f0d241a7ffe41c1e58c190c0b7e59a363
Wagner Souza <wagnersza@corp.globo.com>
2015-08-13 13:07:59 -0300

    Moved to https://github.com/tsuru/dockerized-setup

--- router/run.sh
+++
@@ -1,3 +0,0 @@
-/etc/init.d/redis-server start
-/etc/init.d/hipache start
-/bin/consul-template -consul consul.service.consul:8500 -template "/tmp/redis.ctmpl:/etc/redis/redis.conf:/etc/init.d/redis-server restart"
